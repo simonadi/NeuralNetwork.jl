da6c7a7ad5abfa1ae79d3d5cba37cbf9957a15c4
Simon Adi <viknesh.adi@gmail.com>
2020-07-03 12:18:22 +0200

    restructured project. added NeuralNetwork as a module

--- examples/test.jl
+++ examples/test.jl
@@ -1,13 +1,13 @@
-include("network.jl")
+using .NeuralNetwork
 using CSV
 using Profile
 
-df = CSV.read("input.csv", header=false)
+df = CSV.read("examples/test_set/input.csv", header=false)
 input = convert(Matrix{Float32}, df)
 
 input = [input[i,:] for i in 1:2000]
 
-df = CSV.read("labels.csv", header=false)
+df = CSV.read("examples/test_set/labels.csv", header=false)
 labels = convert(Matrix{Float32}, df)
 
 labels = [[labels[i,1]] for i in 1:2000]
---
+++ src/NeuralNetwork.jl
@@ -0,0 +1,6 @@
+module NeuralNetwork
+include("network.jl")
+
+export Dense, Sigmoid, BinaryCrossEntropy, Network
+export train
+end
